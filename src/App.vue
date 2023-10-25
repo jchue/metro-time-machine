b393ca1b52d75771b286a0909ecdce5de83d7375
Jonathan Chue <5141427+jchue@users.noreply.github.com>
2023-10-24 21:18:38 -0700

    Add GitHub link

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterLink, RouterView } from 'vue-router';
+
+const githubRepo = import.meta.env.VITE_GITHUB_REPO;
 </script>
 
 <template>
@@ -11,6 +13,12 @@ import { RouterLink, RouterView } from 'vue-router'
       <RouterLink to="/map">Map</RouterLink>
       <RouterLink to="/about">About</RouterLink>
     </nav>
+
+    <a v-bind:href="`https://github.com/${githubRepo}`" target="_blank" v-if="githubRepo">
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-github" viewBox="0 0 16 16">
+        <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"/>
+      </svg>
+    </a>
   </header>
 
   <main>
@@ -20,6 +28,7 @@ import { RouterLink, RouterView } from 'vue-router'
 
 <style scoped>
 header {
+  align-items: center;
   display: flex;
 }
 
@@ -31,18 +40,22 @@ header {
 }
 
 nav {
+  flex-grow: 1;
   font-size: 12px;
 }
 
-nav a {
+header a {
   color: var(--color-text);
-  display: inline-block;
-  padding: 0 1rem;
+  display: inline-block; 
 }
 
-nav a:hover {
+header a:hover {
   color: hsla(160, 100%, 37%, 1);
   background-color: transparent;
+}
+
+nav a {
+  padding: 0 1rem;
 }
 
 nav a.router-link-exact-active {
